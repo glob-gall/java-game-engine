8788f0356e5a061a5f34e4ef5be60b9841b50dd7
glob-gall <luisfelipegalleguillos@hotmail.com>
2022-09-26 23:15:14 -0300

    init

---
+++ src/Text.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Text {
+    public String text;
+    public Font font;
+    public double x,y;
+
+    public Text(String text, Font font, double x, double y){
+        this.text = text;
+        this.font = font;
+        this.x = x;
+        this.y = y;
+    }
+}
